43f450e2c2d569c8da68aebc5b136c7f914dabca
= <abdozurghani@gmail.com>
2024-12-06 08:14:33 -0500

    components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import CardList from "./components/CardList/CardList";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const cardData = [
+    { title: "Card 1", description: "Description for card 1" },
+    { title: "Card 2", description: "Description for card 2" },
+    { title: "Card 3", description: "Description for card 3" },
+    { title: "Card 4", description: "Description for card 4" },
+    { title: "Card 5", description: "Description for card 5" },
+    { title: "Card 6", description: "Description for card 6" },
+    { title: "Card 7", description: "Description for card 7" },
+    { title: "Card 8", description: "Description for card 8" },
+    { title: "Card 9", description: "Description for card 9" },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Card List</h1>
+      <CardList cards={cardData} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    outDir:'../dist/artifact',
+    rollupOptions:{
+      input:'src/Export.ts',
+      output:{
+        entryFileNames:'index.js',
+        assetFileNames:'index.css',
+      }
+    }
+  }
 })
